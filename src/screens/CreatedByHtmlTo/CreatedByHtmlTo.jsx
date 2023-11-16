bc19c9b6838f48e380ce024a7b0535e354fa82c0
jsyash <82034076+jsyash@users.noreply.github.com>
2023-11-16 23:00:49 +0530

    Add files via upload

---
+++ src/screens/CreatedByHtmlTo/CreatedByHtmlTo.jsx
@@ -0,0 +1,491 @@
+import React from "react";
+import "./style.css";
+
+export const CreatedByHtmlTo = () => {
+  return (
+    <div className="created-by-html-to">
+      <div className="div">
+        <div className="div-content-wrapper">
+          <div className="div-card">
+            <div className="div-card-body">
+              <div className="heading">
+                <div className="text-wrapper">Customer KYC</div>
+              </div>
+              <div className="div-mb">
+                <div className="span">
+                  <div className="text-wrapper-2">Basic Information</div>
+                </div>
+                <div className="span-step">
+                  <div className="text-wrapper-3">Account Information</div>
+                </div>
+                <div className="div-wrapper">
+                  <div className="text-wrapper-3">KYC Information</div>
+                </div>
+                <div className="span-2">
+                  <div className="text-wrapper-3">Agreement</div>
+                </div>
+              </div>
+              <div className="form">
+                <div className="div-row">
+                  <div className="div-col-md">
+                    <div className="div-form-group">
+                      <label className="text-wrapper-4" htmlFor="input-2">
+                        Company Name
+                      </label>
+                      <div className="input">
+                        <input className="div-placeholder" id="input-2" placeholder="Company Name" type="text" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="div-form-group-wrapper">
+                    <div className="div-form-group">
+                      <label className="text-wrapper-4" htmlFor="input-4">
+                        Email Address
+                      </label>
+                      <div className="r-wrapper">
+                        <input className="r" id="input-4" placeholder="rajeevyash24@gmail.com" type="email" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-row">
+                  <div className="div-col-md">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">Website URL</div>
+                      <div className="input-2">
+                        <div className="div-2">
+                          <div className="text-wrapper-5">www</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="div-col-md-2">
+                    <div className="div-form-group-2">
+                      <div className="label-mobile-number">Mobile Number</div>
+                      <div className="div-input-group">
+                        <div className="div-input-group-wrapper">
+                          <div className="options-wrapper">
+                            <div className="options">
+                              <div className="div-3">
+                                <div className="text-wrapper-6">+91</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="overlap-group-wrapper">
+                          <div className="overlap-group">
+                            <input className="div-placeholder-2" placeholder="Phone Number" type="tel" />
+                            <div className="div-text-field">
+                              <div className="div-editing-view" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-row">
+                  <div className="div-col-md">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">GST Number</div>
+                      <div className="input-2">
+                        <div className="div-4">
+                          <div className="text-wrapper-5">07AAGFF2194N1Z1</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="div-form-group-wrapper">
+                    <div className="div-form-group">
+                      <p className="text-wrapper-4">Please Upload Your GST Certificate</p>
+                      <div className="input-3">
+                        <div className="input-4">
+                          <div className="text-wrapper-7">Choose File</div>
+                        </div>
+                        <div className="text-wrapper-8">No file chosen</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-row">
+                  <div className="div-col-md">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">Choose Company Logo</div>
+                      <div className="input-3">
+                        <div className="input-4">
+                          <div className="text-wrapper-7">Choose File</div>
+                        </div>
+                        <div className="text-wrapper-8">No file chosen</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="div-form-group-wrapper">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">Enter Your PAN Number</div>
+                      <div className="input">
+                        <div className="div-placeholder-3">
+                          <div className="text-wrapper-9">PAN Number</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-row">
+                  <div className="div-col-md">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">Enter Street Name</div>
+                      <div className="input-2">
+                        <div className="div-5">
+                          <div className="text-wrapper-5">India</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="div-form-group-wrapper">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">Enter Zip Code</div>
+                      <div className="input-2">
+                        <div className="div-6">
+                          <div className="text-wrapper-5">462001</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-row">
+                  <div className="div-col-md">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">City Name</div>
+                      <div className="input">
+                        <div className="div-7">
+                          <div className="text-wrapper-5">BHOPAL</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="div-form-group-wrapper">
+                    <div className="div-form-group">
+                      <div className="text-wrapper-4">State Name</div>
+                      <div className="input">
+                        <div className="div-8">
+                          <div className="text-wrapper-5">MADHYA PRADESH</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="div-9">
+                <button className="button-wrapper">
+                  <button className="button">
+                    <div className="next">NEXT</div>
+                  </button>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="aside">
+          <div className="list">
+            <a
+              className="item-link"
+              href="https://www.shipease.in/seller-dashboard"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a
+                className="symbol"
+                href="https://www.shipease.in/seller-dashboard"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                󰨇
+              </a>
+              <a
+                className="text-wrapper-10"
+                href="https://www.shipease.in/seller-dashboard"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Dashboard
+              </a>
+            </a>
+            <a className="item-link" href="https://www.shipease.in/my-orders" rel="noopener noreferrer" target="_blank">
+              <a className="symbol" href="https://www.shipease.in/my-orders" rel="noopener noreferrer" target="_blank">
+                󰬳
+              </a>
+              <a
+                className="text-wrapper-11"
+                href="https://www.shipease.in/my-orders"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Orders
+              </a>
+            </a>
+            <div className="item-link-2">
+              <div className="symbol">󰱖</div>
+              <div className="more-on-orders">
+                More On
+                <br />
+                Orders
+              </div>
+            </div>
+            <a
+              className="item-link"
+              href="https://www.shipease.in/ndr-orders"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a className="symbol" href="https://www.shipease.in/ndr-orders" rel="noopener noreferrer" target="_blank">
+                󱐪
+              </a>
+              <a
+                className="text-wrapper-12"
+                href="https://www.shipease.in/ndr-orders"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Shipments
+              </a>
+            </a>
+            <a
+              className="item-link"
+              href="https://www.shipease.in/my-channels"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a
+                className="symbol"
+                href="https://www.shipease.in/my-channels"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                󰌷
+              </a>
+              <a
+                className="text-wrapper-13"
+                href="https://www.shipease.in/my-channels"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Channels
+              </a>
+            </a>
+            <a className="item-link-3" href="https://www.shipease.in/oms" rel="noopener noreferrer" target="_blank">
+              <a className="symbol-2" href="https://www.shipease.in/oms" rel="noopener noreferrer" target="_blank">
+                󰮆
+              </a>
+              <a
+                className="text-wrapper-14"
+                href="https://www.shipease.in/oms"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                OMS
+              </a>
+            </a>
+            <a className="item-link" href="https://www.shipease.in/billing" rel="noopener noreferrer" target="_blank">
+              <a className="symbol" href="https://www.shipease.in/billing" rel="noopener noreferrer" target="_blank">
+                󱁼
+              </a>
+              <a
+                className="text-wrapper-15"
+                href="https://www.shipease.in/billing"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Billing
+              </a>
+            </a>
+            <a className="item-link-4" href="https://www.shipease.in/weight" rel="noopener noreferrer" target="_blank">
+              <div className="overlap-group-2">
+                <a className="symbol-3" href="https://www.shipease.in/weight" rel="noopener noreferrer" target="_blank">
+                  
+                </a>
+                <a
+                  className="weight-reco"
+                  href="https://www.shipease.in/weight"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Weight
+                  <br />
+                  Reco.
+                </a>
+              </div>
+            </a>
+            <a
+              className="item-link"
+              href="https://www.shipease.in/seller-settings"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a
+                className="symbol"
+                href="https://www.shipease.in/seller-settings"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                󰭒
+              </a>
+              <a
+                className="text-wrapper-16"
+                href="https://www.shipease.in/seller-settings"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Customer
+              </a>
+            </a>
+            <a
+              className="item-link-5"
+              href="https://www.shipease.in/seller-settings_partner"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a
+                className="symbol-4"
+                href="https://www.shipease.in/seller-settings_partner"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                
+              </a>
+              <a
+                className="text-wrapper-17"
+                href="https://www.shipease.in/seller-settings_partner"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Tools
+              </a>
+            </a>
+            <a
+              className="item-link"
+              href="https://www.shipease.in/mis_report"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a className="symbol" href="https://www.shipease.in/mis_report" rel="noopener noreferrer" target="_blank">
+                󰶋
+              </a>
+              <a
+                className="text-wrapper-18"
+                href="https://www.shipease.in/mis_report"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                MIS
+              </a>
+            </a>
+            <a
+              className="item-link-6"
+              href="https://www.shipease.in/customer_support"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <a
+                className="symbol"
+                href="https://www.shipease.in/customer_support"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                󰵰
+              </a>
+              <a
+                className="customer-support"
+                href="https://www.shipease.in/customer_support"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Customer
+                <br />
+                Support
+              </a>
+            </a>
+          </div>
+        </div>
+        <header className="header">
+          <div className="div-mr-auto-margin">
+            <div className="div-mr-auto">
+              <a
+                className="link"
+                href="https://www.shipease.in/seller-dashboard"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <div className="element" />
+              </a>
+            </div>
+          </div>
+          <div className="div-header-top">
+            <div className="div-header-top-2">
+              <div className="welcome-rajeev-yash-wrapper">
+                <div className="welcome-rajeev-yash">WELCOME  RAJEEV YASH (SE-118944)</div>
+              </div>
+              <div className="symbol-wrapper">
+                <div className="symbol-5"></div>
+              </div>
+              <div className="link-2">
+                <div className="symbol-5"></div>
+              </div>
+              <a
+                className="link-3"
+                href="https://www.shipease.in/seller-profile"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <a
+                  className="symbol-5"
+                  href="https://www.shipease.in/seller-profile"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  
+                </a>
+              </a>
+              <a
+                className="link-4"
+                href="https://www.shipease.in/seller-change-password"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <a
+                  className="symbol-5"
+                  href="https://www.shipease.in/seller-change-password"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  
+                </a>
+              </a>
+              <div className="link-5">
+                <div className="symbol-6"></div>
+                <div className="element-2">{"  "}₹0</div>
+              </div>
+              <a
+                className="link-6"
+                href="https://www.shipease.in/seller-logout"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <a
+                  className="symbol-5"
+                  href="https://www.shipease.in/seller-logout"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  
+                </a>
+              </a>
+              <button className="button-2">
+                <div className="symbol-5"></div>
+              </button>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/CreatedByHtmlTo/index.js
@@ -0,0 +1,1 @@
+export { CreatedByHtmlTo } from "./CreatedByHtmlTo";
